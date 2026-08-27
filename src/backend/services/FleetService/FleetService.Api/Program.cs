e6303704358a4bc3f52b9d8c20eb9895215ecf8c
ssrannj <jeyanthansaran@gmail.com>
2026-08-27 18:42:52 +0530

    feat: add PostgreSQL and EF Core foundation

--- src/backend/services/FleetService/FleetService.Api/Program.cs
+++ src/backend/services/FleetService/FleetService.Api/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using FleetService.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<FleetDbContext>(options =>
+    options.UseNpgsql(GetDatabaseConnectionString(builder.Configuration)));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -21,3 +26,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static string GetDatabaseConnectionString(IConfiguration configuration)
+{
+    var host = configuration["DB_HOST"] ?? "localhost";
+    var port = configuration["DB_PORT"] ?? "5432";
+    var user = configuration["DB_USER"] ?? "postgres";
+    var password = configuration["DB_PASSWORD"] ?? "your_password_here";
+    var dbName = configuration["FLEET_DB_NAME"] ?? "fleetflow_fleet";
+
+    return $"Host={host};Port={port};Database={dbName};Username={user};Password={password};";
+}
--- src/backend/services/IdentityService/IdentityService.Api/Program.cs
+++ src/backend/services/IdentityService/IdentityService.Api/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using IdentityService.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseNpgsql(GetDatabaseConnectionString(builder.Configuration)));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -21,3 +26,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static string GetDatabaseConnectionString(IConfiguration configuration)
+{
+    var host = configuration["DB_HOST"] ?? "localhost";
+    var port = configuration["DB_PORT"] ?? "5432";
+    var user = configuration["DB_USER"] ?? "postgres";
+    var password = configuration["DB_PASSWORD"] ?? "your_password_here";
+    var dbName = configuration["AUTH_DB_NAME"] ?? "fleetflow_auth";
+
+    return $"Host={host};Port={port};Database={dbName};Username={user};Password={password};";
+}
